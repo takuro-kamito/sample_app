88ef907d12cb2c416c531fb4d8360a4b81f7fe0f
EC2 Default User <153484664+takuro-kamito@users.noreply.github.com>
2024-01-14 13:40:05 +0000

    [Finish] 8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,13 +6,13 @@ class ListsController < ApplicationController
   
   def create
     
-    list = List.new(list_params)
-    
-    list.save
-    
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(list.id)
+    else
+      render :new
   end
-
+end 
   def index
     @lists = List.all 
   end
@@ -43,4 +43,5 @@ private
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
-  end
+end 
+  
